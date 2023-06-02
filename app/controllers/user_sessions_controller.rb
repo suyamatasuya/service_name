0650ec24b1004bd013c222a4b1e2a95ad3ed943b
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-06-02 22:46:12 +0900

    ヘッダー画面とフッター画面の画面作成

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,10 @@
+class UserSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def de
+  end
+end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import '../stylesheets/application';
+import '../stylesheets/custom.scss';
 
 Rails.start()
 Turbolinks.start()
