230725341b2025c87a88221e22e93f6d8d7eba06
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-06-30 14:42:36 +0900

    ユーザー登録画面、ログイン画面のモーダルウィンドウ化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import 'jquery'; // ここでjQueryをインポート
 import 'bootstrap';
 import '../stylesheets/application';
 import '../stylesheets/custom.scss';
