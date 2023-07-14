37a1f8cf6965b04950e0f07808311824dd5e66a9
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-07-14 13:56:45 +0900

    本番環境エラーの修正

--- db/migrate/20230624122616_create_join_table_symptom_care_method.rb
+++ db/migrate/20230624122616_create_join_table_symptom_care_method.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableSymptomCareMethod < ActiveRecord::Migration[6.1]
   def change
-    create_table :symptoms_care_methods, id: false do |t|
+    create_table :care_methods_symptoms, id: false do |t|
       t.belongs_to :symptom
       t.belongs_to :care_method
     end
