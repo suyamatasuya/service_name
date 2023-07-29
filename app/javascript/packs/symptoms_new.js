4c913d5cb55811e8e1863851aff3225987b7eb33
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-07-29 15:54:26 +0900

    バグ修正

--- app/javascript/packs/symptoms_new.js
+++ app/javascript/packs/symptoms_new.js
@@ -1,13 +1,12 @@
 document.addEventListener('turbolinks:load', function(){
-  console.log("Turbolinks:load event triggered"); // これを追加
-  document.querySelectorAll('.custom-label').forEach(function(label){
-      label.addEventListener('click', function(e){
-          // Clear previous selection
-          document.querySelectorAll('.custom-label').forEach(function(otherLabel){
-              otherLabel.classList.remove('active');
-          })
-          // Set the clicked label as active
-          e.currentTarget.classList.add('active');
-      })
-  })
+    document.querySelectorAll('.custom-label').forEach(function(label){
+        label.addEventListener('click', function(e){
+            // Clear previous selection
+            document.querySelectorAll('.custom-label').forEach(function(otherLabel){
+                otherLabel.classList.remove('active');
+            })
+            // Set the clicked label as active
+            e.currentTarget.classList.add('active');
+        })
+    })
 })
