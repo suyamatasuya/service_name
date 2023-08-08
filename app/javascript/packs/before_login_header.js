1bf0a2ae1d65f076db306eb90f64cd37916db2f8
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-08-09 07:15:00 +0900

    レスポンシブ化を進めました

--- app/javascript/packs/before_login_header.js
+++ app/javascript/packs/before_login_header.js
@@ -1,29 +1,31 @@
 document.addEventListener("turbolinks:load", (event) => {
   const hamburger = document.querySelector(".hamburger");
-  const sideBar = document.querySelector(".side-bar");
+  const sideBar = document.querySelector("#mySidebar");
   const navbarLinks = document.querySelectorAll(".nav-link");
 
-  hamburger.addEventListener("click", () => {
-    sideBar.style.width = sideBar.style.width === '250px' ? '0' : '250px';
-    for (let link of navbarLinks) {
-      link.style.display = sideBar.style.width === '250px' ? 'block' : 'none';
-    }
-  });
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-      sideBar.style.width = '0';
+  if (hamburger && sideBar && navbarLinks.length > 0) {
+    hamburger.addEventListener("click", () => {
+      sideBar.style.width = sideBar.style.width === '250px' ? '0' : '250px';
       for (let link of navbarLinks) {
-        link.style.display = 'block';
-        link.classList.remove("nav-link");
+        link.style.display = sideBar.style.width === '250px' ? 'block' : 'none';
       }
-    } else {
-      if (sideBar.style.width === '250px') {
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 768) {
+        sideBar.style.width = '0';
         for (let link of navbarLinks) {
-          link.style.display = 'none';
-          link.classList.add("nav-link");
+          link.style.display = 'block';
+          link.classList.remove("nav-link");
+        }
+      } else {
+        if (sideBar.style.width === '250px') {
+          for (let link of navbarLinks) {
+            link.style.display = 'none';
+            link.classList.add("nav-link");
+          }
         }
       }
-    }
-  });
+    });
+  }
 });
