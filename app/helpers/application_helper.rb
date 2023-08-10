ce09eb55f940db39f9b144edf907c93a9050888f
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2023-08-10 20:19:15 +0900

    youtube動画を埋め込み形式に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+    def extract_video_id(url)
+      if url.include?("youtu.be")
+        url.split('/').last
+      else
+        url.split('=').last
+      end
+    end
+  end
+  
