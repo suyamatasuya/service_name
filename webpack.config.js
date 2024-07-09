8342d17a80141826b1b9e7f6b5ba0f889b36b392
suyamatatsuya <suyama.baseball1025@docomo.ne.jp>
2024-07-09 14:59:44 +0900

    修正

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // or 'production'
+  entry: './src/index.js', // プロジェクトのエントリーポイントに合わせて変更
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  node: {
+    __dirname: true,
+    __filename: true,
+    global: true
+  }
+};
